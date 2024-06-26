a7495c779c55246052e768158ad64526ce27007c
lucaskauer <lucasgkauer@gmail.com>
2024-06-26 16:36:14 +0000

    add validation and redirect

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,46 @@
 # myapp/views.py
 from .forms import ArtistaForm, LancamentoForm
 from .models import Artista, Lancamento
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def manifesto(request):
+
+    return render(request, 'myapp/manifesto.html')
+
+@login_required(login_url='/')
+def visualizar_lancamento(request, pk):
+    try:
+        lancamento = Lancamento.objects.get(pk=pk)
+    except Lancamento.DoesNotExist:
+        return render(request, 'myapp/registro_nao_encontrado.html', {'mensagem': 'Lançamento não encontrado.'})
+
+    return render(request, 'myapp/visualizar_lancamento.html', {'lancamento': lancamento})
+
+def visualizar_artista(request, pk):
+
+    try:
+        artista = Artista.objects.get(pk=pk)
+        lancamentos = Lancamento.objects.filter(artista=artista)
+    except Artista.DoesNotExist:
+        return render(request, 'myapp/registro_nao_encontrado.html', {'mensagem': 'Artista não encontrado.'})
+
+    return render(request, 'myapp/visualizar_artista.html', {'artista': artista, 'lancamentos': lancamentos})
+
+@login_required(login_url='/')
+def painel_geral(request):
+
+    artistas = Artista.objects.filter(usuario=request.user)
+    lancamentos = Lancamento.objects.filter(usuario=request.user)
+
+    return render(request, 'myapp/painel.html', {'artistas': artistas, 'lancamentos': lancamentos})
 
 @login_required(login_url='/')
 def cadastrar_artista(request):
@@ -24,21 +58,6 @@ def cadastrar_artista(request):
     return render(request, 'myapp/cadastrar_artista.html', {'form': form})
 
 @login_required(login_url='/')
-def editar_artista(request, pk):
-    artista = get_object_or_404(Artista, pk=pk, usuario=request.user)
-    if request.method == 'POST':
-        form = ArtistaForm(request.POST, request.FILES, instance=artista)  # Adicione request.FILES aqui
-        if form.is_valid():
-            artista = form.save(commit=False)
-            artista.usuario = request.user
-            artista.save()
-            messages.success(request, 'Alteração realizada com sucesso.')
-            return redirect('painel_geral')
-    else:
-        form = ArtistaForm(instance=artista)
-    return render(request, 'myapp/editar_artista.html', {'form': form})
-
-@login_required(login_url='/')
 def cadastrar_lancamento(request):
     if request.method == 'POST':
         form = LancamentoForm(request.POST, request.FILES, user=request.user)
@@ -52,37 +71,6 @@ def cadastrar_lancamento(request):
         form = LancamentoForm(user=request.user)
 
     return render(request, 'myapp/cadastrar_lancamento.html', {'form': form})
-
-@login_required(login_url='/')
-def editar_lancamento(request, pk):
-    lancamento = get_object_or_404(Lancamento, pk=pk, usuario=request.user)
-    if request.method == 'POST':
-        form = LancamentoForm(request.POST, request.FILES, instance=lancamento)  # Adicione request.FILES aqui
-        if form.is_valid():
-            lancamento = form.save(commit=False)
-            lancamento.usuario = request.user
-            lancamento.save()
-            messages.success(request, 'Alteração realizada com sucesso.')
-            return redirect('painel_geral')
-    else:
-        form = LancamentoForm(instance=lancamento, user=request.user)
-    return render(request, 'myapp/editar_lancamento.html', {'form': form})
-
-@login_required(login_url='/')
-def excluir_artista(request, pk):
-    artista = get_object_or_404(Artista, pk=pk, usuario=request.user)
-    if request.method == 'POST':
-        artista.delete()
-        return redirect('painel_geral')
-    return render(request, 'myapp/confirmar_exclusao.html', {'artista': artista})
-
-@login_required(login_url='/')
-def excluir_lancamento(request, pk):
-    lancamento = get_object_or_404(Lancamento, pk=pk, usuario=request.user)
-    if request.method == 'POST':
-        lancamento.delete()
-        return redirect('painel_geral')
-    return render(request, 'myapp/confirmar_exclusao_lancamento.html', {'lancamento': lancamento})
 
 def listar_artistas(request):
     artista_list = Artista.objects.all()
@@ -130,26 +118,67 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'myapp/signup.html', {'form': form})
 
-def index(request):
-    return render(request, 'myapp/index.html')
 
 @login_required(login_url='/')
-def painel_geral(request):
-    artistas = Artista.objects.filter(usuario=request.user)
-    lancamentos = Lancamento.objects.filter(usuario=request.user)
+def editar_artista(request, pk):
 
-    return render(request, 'myapp/painel.html', {'artistas': artistas, 'lancamentos': lancamentos})
+    try:
+        artista = Artista.objects.get(pk=pk, usuario=request.user)
+    except Artista.DoesNotExist:
+        return render(request, 'myapp/registro_nao_encontrado.html', {'mensagem': 'Artista não encontrado.'})
 
-def visualizar_artista(request, pk):
-    artista = get_object_or_404(Artista, pk=pk)
-    lancamentos = Lancamento.objects.filter(artista=artista)
-    return render(request, 'myapp/visualizar_artista.html', {'artista': artista, 'lancamentos': lancamentos})
+    if request.method == 'POST':
+        form = ArtistaForm(request.POST, request.FILES, instance=artista)  # Adicione request.FILES aqui
+        if form.is_valid():
+            artista = form.save(commit=False)
+            artista.usuario = request.user
+            artista.save()
+            messages.success(request, 'Alteração realizada com sucesso.')
+            return redirect('painel_geral')
+    else:
+        form = ArtistaForm(instance=artista)
+    return render(request, 'myapp/editar_artista.html', {'form': form})
 
 @login_required(login_url='/')
-def visualizar_lancamento(request, pk):
-    lancamento = get_object_or_404(Lancamento, pk=pk)
-    return render(request, 'myapp/visualizar_lancamento.html', {'lancamento': lancamento})
+def editar_lancamento(request, pk):
 
-def manifesto(request):
+    try:
+        lancamento = Lancamento.objects.get(pk=pk, usuario=request.user)
+    except Lancamento.DoesNotExist:
+        return render(request, 'myapp/registro_nao_encontrado.html', {'mensagem': 'Lancamento não encontrado.'})
 
-    return render(request, 'myapp/manifesto.html')
+    if request.method == 'POST':
+        form = LancamentoForm(request.POST, request.FILES, instance=lancamento)  # Adicione request.FILES aqui
+        if form.is_valid():
+            lancamento = form.save(commit=False)
+            lancamento.usuario = request.user
+            lancamento.save()
+            messages.success(request, 'Alteração realizada com sucesso.')
+            return redirect('painel_geral')
+    else:
+        form = LancamentoForm(instance=lancamento, user=request.user)
+    return render(request, 'myapp/editar_lancamento.html', {'form': form})
+
+@login_required(login_url='/')
+def excluir_artista(request, pk):
+    try:
+        artista = Artista.objects.get(pk=pk, usuario=request.user)
+    except Artista.DoesNotExist:
+        return render(request, 'myapp/registro_nao_encontrado.html', {'mensagem': 'Artista não encontrado.'})
+
+    if request.method == 'POST':
+        artista.delete()
+        return redirect('painel_geral')
+    return render(request, 'myapp/confirmar_exclusao.html', {'artista': artista})
+
+@login_required(login_url='/')
+def excluir_lancamento(request, pk):
+    try:
+        lancamento = Lancamento.objects.get(pk=pk, usuario=request.user)
+    except Lancamento.DoesNotExist:
+        return render(request, 'myapp/registro_nao_encontrado.html', {'mensagem': 'Lancamento não encontrado.'})
+
+    if request.method == 'POST':
+        lancamento.delete()
+        return redirect('painel_geral')
+    return render(request, 'myapp/confirmar_exclusao_lancamento.html', {'lancamento': lancamento})
