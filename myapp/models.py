f626633562fb049febbfac4b566d50b05713df28
lucaskauer <lucasgkauer@gmail.com>
2024-08-04 13:17:32 +0000

    v1 update

--- myapp/models.py
+++ myapp/models.py
@@ -34,7 +34,7 @@ class Artista(models.Model):
 class Som(models.Model):
     titulo = models.CharField(max_length=200)
     imagem_som = models.ImageField(upload_to='sons/', blank=True, null=True)
-    audio = models.FileField(upload_to='audios/', validators=[FileExtensionValidator(allowed_extensions=['mp3', 'wav'])], blank=True, null=True)
+    audio = models.FileField(upload_to='audios/', validators=[FileExtensionValidator(allowed_extensions=['mp3'])], blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
     descricao = models.TextField(blank=True, null=True)
